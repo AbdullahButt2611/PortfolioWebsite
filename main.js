b1ad0b0cc40c3af5ae9a36a21e0235d289f511c3
M. Abdullah Butt <81871162+AbdullahButt2611@users.noreply.github.com>
2022-07-22 21:48:33 +0500

    Complete
    The AOS Library and all the necessary functionalities has been added into this web portfolio.

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+const htmlVar = document.querySelector("html");
+
 $(document).ready(function(){
     $('.menu-toggler').on('click', function(){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('#up').on('click', function(){
+        htmlVar.style.scrollBehavior = "smooth";
+        window.scrollTo(0,0);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
+
+    
 });
